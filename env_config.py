1ea3452f35c0c27e3af98d144e9798b4a8c5e963
RickFort <42819509+RickFort@users.noreply.github.com>
2025-05-06 00:20:30 +0200

    Create env_config.py

---
+++ env_config.py
@@ -0,0 +1,7 @@
+OPENAI_API_KEY='<openai-key>'
+LLM_MODEL="gpt-3.5-turbo"
+EMBEDDING_MODEL="text-embedding-3-small"
+DB_PATH="/tmp/"
+DB_NAME="supply_chain.db"
+SQL_AGENT_FEW_SHOT=True
+FILE_ID = '<file-id>'
